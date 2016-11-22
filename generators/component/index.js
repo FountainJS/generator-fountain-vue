38d0b3165b435c45a0b9287fd2e4120e4c2887f6
Micaël Mbagira <micael.mbagira@icloud.com>
2016-11-22 10:02:08 +0100

    Replace typings with @types

--- generators/component/index.js
+++ generators/component/index.js
@@ -6,15 +6,13 @@ module.exports = fountain.Base.extend({
     const titleCase = string => string.charAt(0).toUpperCase() + string.slice(1);
     const lowerCase = string => string.charAt(0).toLowerCase() + string.slice(1);
     const path = this.options.dir ? `app/${this.options.dir}` : `app`;
-    const typings = this.options.dir ? this.options.dir.split('/').reduce(prev => `../${prev}`, '../../typings/index.d.ts') : '../../typings/index.d.ts';
     const props = {
       componentName: lowerCase(name),
       className: titleCase(name),
       modules: this.config.get('props').modules,
       js: 'babel',
       framework: 'vue',
-      name,
-      typings
+      name
     };
     this.copyTemplate(`src/app/component.vue`, `src/${path}/${name}.vue`, props);
     this.copyTemplate(`src/app/component.spec.js`, `src/${path}/${name}.spec.js`, props);
